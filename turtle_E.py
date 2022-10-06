3cded336275ba08a6cd468b191885f3953f26bc8
florian <34652@ma-web.nl>
2022-10-06 10:31:19 +0200

    turtle

---
+++ turtle_E.py
@@ -0,0 +1,20 @@
+import turtle
+
+turtle.bgcolor("black")
+turtle.speed(100)
+for x in range (72):
+    turtle.color("red")
+    turtle.circle(100)
+    turtle.right(5)
+
+for x in range (72):
+    turtle.color("lime green")
+    turtle.circle(125)
+    turtle.right(5)
+
+for x in range (72):
+    turtle.color("blue")
+    turtle.circle(150)
+    turtle.right(5)
+
+turtle.done()
